c2214d62f2ee367a8fd77ccbfb7be66b61fd22c5
ls zr <wsyglszr@gmail.com>
2017-11-12 21:14:04 +0800

    test update

--- app/src/main/java/cn/abtion/neuqercc/home/fragments/HomeFragment.java
+++ app/src/main/java/cn/abtion/neuqercc/home/fragments/HomeFragment.java
@@ -2,27 +2,34 @@ package cn.abtion.neuqercc.home.fragments;
 
 
 import android.content.Intent;
-import android.provider.ContactsContract;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.abtion.neuqercc.R;
 import cn.abtion.neuqercc.base.adapters.BaseRecyclerViewAdapter;
 import cn.abtion.neuqercc.base.fragments.BaseFragment;
 import cn.abtion.neuqercc.home.activities.CompetitionActivity;
 import cn.abtion.neuqercc.home.adapters.HomeAdapter;
 import cn.abtion.neuqercc.home.models.ContestListModel;
-import cn.abtion.neuqercc.main.MainActivity;
+
+import static cn.abtion.neuqercc.common.Config.FLING_MIN_DISTANCE;
+import static cn.abtion.neuqercc.common.Config.FLING_MIN_VELOCITY;
 
 
 /**
@@ -33,6 +40,9 @@ import cn.abtion.neuqercc.main.MainActivity;
 
 public class HomeFragment extends BaseFragment {
 
+    @BindView(R.id.vf_contest)
+    ViewFlipper vfContest;
+    Unbinder unbinder;
     private ArrayList<ContestListModel> contestListModels;
 
 
@@ -58,10 +68,85 @@ public class HomeFragment extends BaseFragment {
     @Override
     protected void initView() {
 
+
+        vfContest.addView(getImageView(R.drawable.img_home_view));
+        vfContest.addView(getImageView(R.drawable.ic_register_password));
+        vfContest.addView(getImageView(R.drawable.ic_back));
+        vfContest.addView(getImageView(R.drawable.ic_contest_before_title));
+        vfContest.setInAnimation(inFromRightAnimation());
+        vfContest.setOutAnimation(outToLeftAnimation());
+        vfContest.setFlipInterval(2000);
+        vfContest.startFlipping();
+        vfContest.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return HomeFragment.this.gestureDetector.onTouchEvent(event);
+            }
+        });
+
+        gestureDetector = new GestureDetector(this.getContext(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                vfContest.stopFlipping();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        vfContest.startFlipping();
+                    }
+                }, 3000);
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+                if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE &&
+                        Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+                    vfContest.setInAnimation(inFromRightAnimation());
+                    vfContest.setOutAnimation(outToLeftAnimation());
+                    vfContest.showNext();
+                    vfContest.setInAnimation(inFromRightAnimation());
+                    vfContest.setOutAnimation(outToLeftAnimation());
+                    vfContest.startFlipping();
+                } else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE &&
+                        Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+                    vfContest.setInAnimation(inFromLeftAnimation());
+                    vfContest.setOutAnimation(outToRightAnimation());
+                    vfContest.showPrevious();
+                    vfContest.setInAnimation(inFromRightAnimation());
+                    vfContest.setOutAnimation(outToLeftAnimation());
+                    vfContest.startFlipping();
+                }
+                return false;
+            }
+        });
+
+
         recHome.setNestedScrollingEnabled(false);
         contestListModels = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
-            contestListModels.add(new ContestListModel("数学建模", "前端、热血、创意", "比赛时间：2017.10.15-2017.10.18","报名时间：2017.10.15-2017.10.18"));
+            contestListModels.add(new ContestListModel("数学建模", "前端、热血、创意", "比赛时间：2017.10.15-2017.10.18", "报名时间：2017.10.15-2017.10.18"));
         }
 
         HomeAdapter homeAdapter = new HomeAdapter(getContext(), contestListModels);
@@ -73,7 +158,7 @@ public class HomeFragment extends BaseFragment {
 
             @Override
             public void onItemClicked(ContestListModel contestListModel, BaseRecyclerViewAdapter.ViewHolder viewHolder) {
-                Intent intent=new Intent(getContext(),CompetitionActivity.class);
+                Intent intent = new Intent(getContext(), CompetitionActivity.class);
                 startActivity(intent);
             }
         });
@@ -85,4 +170,17 @@ public class HomeFragment extends BaseFragment {
 
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
