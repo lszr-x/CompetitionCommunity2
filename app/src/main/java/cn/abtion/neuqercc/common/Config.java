30dfba73275e179ce25e1d7cd63c2528ad9b622c
ls zr <wsyglszr@gmail.com>
2017-11-12 19:02:31 +0800

    Update

--- app/src/main/java/cn/abtion/neuqercc/common/Config.java
+++ app/src/main/java/cn/abtion/neuqercc/common/Config.java
@@ -21,9 +21,6 @@ public class Config {
     /**
      * APP Server 网络请求BaseUrl
      */
-    /**
-     * public static final String APP_SERVER_BASE_URL = "http://oj.marklux.cn/";
-     */
     public static final String APP_SERVER_BASE_URL = "http://www.thmaoqiu.cn/";
 
     /**
@@ -36,6 +33,20 @@ public class Config {
      */
     public static final int SPLASH_GO_MAIN_DELAY = 1500;
 
+    /**
+     * 密码位数限定
+     */
     public static final int PASSWORD_MIN_LIMIT =6;
     public static final int PASSWORD_MAX_LIMIT =16;
+
+    /**
+     * 最小滑动像素
+     */
+    public static final float FLING_MIN_DISTANCE = 1;
+    /**
+     * 最小滑动速度
+     */
+    public static final float FLING_MIN_VELOCITY = 1;
+
+
 }
--- app/src/main/java/cn/abtion/neuqercc/home/fragments/HomeFragment.java
+++ app/src/main/java/cn/abtion/neuqercc/home/fragments/HomeFragment.java
@@ -1,11 +1,20 @@
 package cn.abtion.neuqercc.home.fragments;
 
 
+import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
 
@@ -15,6 +24,9 @@ import cn.abtion.neuqercc.base.fragments.BaseFragment;
 import cn.abtion.neuqercc.home.adapters.HomeAdapter;
 import cn.abtion.neuqercc.home.models.ContestListModel;
 
+import static cn.abtion.neuqercc.common.Config.FLING_MIN_DISTANCE;
+import static cn.abtion.neuqercc.common.Config.FLING_MIN_VELOCITY;
+
 
 /**
  * @author abtion.
@@ -23,6 +35,11 @@ import cn.abtion.neuqercc.home.models.ContestListModel;
  */
 
 public class HomeFragment extends BaseFragment {
+
+
+    private GestureDetector gestureDetector;    //手势检测
+//    private ViewFlipper vfContest;
+
 
     private ArrayList<ContestListModel> contestListModels;
 
@@ -35,6 +52,9 @@ public class HomeFragment extends BaseFragment {
     RecyclerView recHome;
     @BindView(R.id.view_container)
     FrameLayout viewContainer;
+    @BindView(R.id.vf_contest)
+    ViewFlipper vfContest;
+
 
     @Override
     protected int getLayoutId() {
@@ -48,10 +68,90 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+
+        
+
+        vfContest.addView(getImageView(R.drawable.img_home_view));
+        vfContest.addView(getImageView(R.drawable.ic_register_password));
+        vfContest.addView(getImageView(R.drawable.ic_back));
+        vfContest.addView(getImageView(R.drawable.ic_contest_before_title));
+        vfContest.setInAnimation(inFromRightAnimation());
+        vfContest.setOutAnimation(outToLeftAnimation());
+        vfContest.setFlipInterval(2000);
+        vfContest.startFlipping();
+        vfContest.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return HomeFragment.this.gestureDetector.onTouchEvent(event);
+            }
+        });
+
+        gestureDetector = new GestureDetector(this.getContext(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                vfContest.stopFlipping();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        vfContest.startFlipping();
+                    }
+                }, 3000);
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+                if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE &&
+                        Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+                    vfContest.setInAnimation(inFromRightAnimation());
+                    vfContest.setOutAnimation(outToLeftAnimation());
+                    vfContest.showNext();
+                    vfContest.startFlipping();
+                } else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE &&
+                        Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+                    vfContest.setInAnimation(inFromLeftAnimation());
+                    vfContest.setOutAnimation(outToRightAnimation());
+                    vfContest.showPrevious();
+                    vfContest.setInAnimation(inFromRightAnimation());
+                    vfContest.setOutAnimation(outToLeftAnimation());
+                    vfContest.startFlipping();
+                }
+                return false;
+            }
+        });
+
+
+        /**
+         *
+         * home界面recylerview
+         *
+         */
         recHome.setNestedScrollingEnabled(false);
         contestListModels = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
-            contestListModels.add(new ContestListModel("数学建模", "前端、热血、创意", "比赛时间：2017.10.15-2017.10.18","报名时间：2017.10.15-2017.10.18"));
+            contestListModels.add(new ContestListModel("数学建模", "前端、热血、创意", "比赛时间：2017.10.15-2017.10.18", "报名时间：2017.10.15-2017.10.18"));
         }
         HomeAdapter homeAdapter = new HomeAdapter(getContext(), contestListModels);
         recHome.setAdapter(homeAdapter);
@@ -62,5 +162,84 @@ public class HomeFragment extends BaseFragment {
     protected void loadData() {
 
     }
+
+
+    private ImageView getImageView(int id) {
+        ImageView imageView =new ImageView(this.getContext());
+        imageView.setImageResource(id);
+        return imageView;
+    }
+
+
+    /**
+     *
+     * 轮播图进入退出动画效果
+     *
+     */
+
+    /**
+     * 定义从右侧进入的动画效果
+     *
+     * @return
+     */
+    protected Animation inFromRightAnimation() {
+        Animation inFromRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromRight.setDuration(200);
+        inFromRight.setInterpolator(new AccelerateInterpolator());
+        return inFromRight;
+    }
+
+    /**
+     * 定义从左侧退出的动画效果
+     *
+     * @return
+     */
+    protected Animation outToLeftAnimation() {
+        Animation outtoLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        outtoLeft.setDuration(200);
+        outtoLeft.setInterpolator(new AccelerateInterpolator());
+        return outtoLeft;
+    }
+
+    /**
+     * 定义从左侧进入的动画效果
+     *
+     * @return
+     */
+    protected Animation inFromLeftAnimation() {
+        Animation inFromLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromLeft.setDuration(200);
+        inFromLeft.setInterpolator(new AccelerateInterpolator());
+        return inFromLeft;
+    }
+
+    /**
+     * 定义从右侧退出时的动画效果
+     *
+     * @return
+     */
+    protected Animation outToRightAnimation() {
+        Animation outtoRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        outtoRight.setDuration(200);
+        outtoRight.setInterpolator(new AccelerateInterpolator());
+        return outtoRight;
+    }
+
 
 }
